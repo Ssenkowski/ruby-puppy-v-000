4f16d3e788208de38b672eedeb899fddb5019284
Steven Senkowski <scsenkowski@gmail.com>
2017-11-20 07:07:25 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class puppy
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all.each do |puppy|
+      puts puppy.name
+    end
+  end
+end 
